5bdae42c6101b3fa6b7104d3460f2de02d8cd4bd
theonlyamos <theonlyamos@gmail.com>
2023-12-11 14:08:07 +0000

    v0.3.4-alpha

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from importlib.metadata import entry_points
 from setuptools import setup, find_packages
 
-VERSION = '0.3.3'
+VERSION = '0.3.4'
 
 with open('README.md', 'rt') as file:
     LONG_DESCRIPTION = file.read()
